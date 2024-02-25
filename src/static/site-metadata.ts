9c8ede12d8edf9a3548ee64c03a2ed8d1b0eeb94
XinYu Zhang <zag.zj.cn@gmail.com>
2024-02-25 23:50:36 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,18 +12,18 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Workouts Map',
-  siteUrl: 'https://ben29.xyz',
+  siteUrl: 'https://xancoding.cn/',
   logo: 'https://bu.dusays.com/2023/04/29/644cd8c3b45b9.jpg',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://ben29.xyz',
+      url: 'https://xancoding.cn/',
     },
     {
       name: 'About',
-      url: 'https://github.com/ben-29/workouts_page/blob/master/README-CN.md',
+      url: 'https://github.com/Xancoding/workouts_page/blob/master/README-CN.md',
     },
   ],
 };
